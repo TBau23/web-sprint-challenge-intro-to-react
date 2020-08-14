a9220b8a9d9e5c155a55d3aeb0485f16980b6bf2
Tom Bauer <tbauer1898@gmail.com>
2020-08-14 11:16:47 -0500

    Done with MVP.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,29 @@
-// Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledChar = styled.div`
+    border: 4px solid cyan;
+    background-color: black;
+    color: white;
+    width: 60%;
+    margin: 3% auto;
+
+`
+
+export default function Character(props) {
+
+    const { char, setCharData } = props
+
+    return (
+
+        <StyledChar>
+            <h2>{char.name}</h2>
+            <h3>Status: {char.status}</h3>
+            <h3>{char.species}</h3>
+            <h3>Place of Origin: {char.origin.name}</h3>
+            <img src={char.image}/>
+        </StyledChar>
+
+    )
+
+}
